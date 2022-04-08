1e419358bb12bbcf9ef379d24a490c1b0ba9a790
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-04-08 16:12:22 +0100

    fmt

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "remote" {
-            hostname = "app.terraform.io"
-            organization = "bablido"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "bablido"
 
-            workspaces {
-            name = "vault-secrets"
-            }
+    workspaces {
+      name = "vault-secrets"
     }
+  }
 }
