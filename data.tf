43d0bcf9ba3e0ef6ede420f74981194b9e5947d3
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-08 13:12:26 +0300

    adding iam role policy and policy attachement

--- data.tf
+++ data.tf
@@ -8,3 +8,21 @@ data "aws_ssm_parameter" "access_key"{
 data "aws_ssm_parameter" "secret_key"{
     name = "/app/vault/aws/AWS_SECRET_KEY"
 }
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {
+
+}
+
+data "aws_iam_policy" "policy" {
+  name = "AdministratorAccess"
+}
--- main.tf
+++ main.tf
@@ -7,3 +7,16 @@ resource "vault_aws_secret_backend" "aws" {
     max_lease_ttl_seconds =  7200
   
 }
+
+resource "aws_iam_role" "role" {
+    name= "deplyment-role"
+    assume_role_policy = data.aws_iam_policy_document.policy.json
+    tags = local.tags
+  
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+    role = aws_iam_role.role.name
+    policy_arn = data.aws_iam_policy.policy.arn
+  
+}
